0663ee6af8fad55272646a92176c893105f7fe7e
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-10-05 21:19:14 -0400

    connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/trippztracker';
 
-mongoose.connect(
-  process.env.MONGODB_URI || ' mongodb://127.0.0.1:27017/trippztracker',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
 
-;
+db.on('error', (err) => {
+  console.error('MongoDB Connection Error:', err);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = db;
+
